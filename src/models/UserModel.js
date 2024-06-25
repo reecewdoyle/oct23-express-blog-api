0e72efc468cdd4850b4adbe4354b0d162f8d56a9
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-06-25 19:57:42 +1000

    feat: user model has password and presave

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -17,12 +17,43 @@ const userSchema = mongoose.Schema({
 		required: false, 
 		unique: false
 	},
+	password: {
+		type: String,
+		required: true,
+		unique: false
+	}
+	// isAdmin // boolean for role authorization
 	// comments: {
 	// 	// These are NOT the same comments as what the Blogs contain, they just reuse the comment schema
 	// 	types: [commentSchema],
 	// 	required: false
 	// }
 });
+
+
+userSchema.pre(
+	"save",
+	async function (next) { 
+		const user = this;
+		console.log("Pre-save hook running.");
+		
+		if (!user.isModified("password")){
+			return;
+		}
+		
+		console.log("Pre-save hook running and password is modified!");
+		// if we reach this line of code, the password is modified
+		// and thus is note encrypted!
+		// we must encrypt it!
+
+		// TODO: Encryption
+
+		
+		next();
+	}
+)
+
+
 
 
 const UserModel = mongoose.model("User", userSchema);
