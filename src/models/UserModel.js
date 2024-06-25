75a93626cb07e8d23d52b458f0907faaa1a15659
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-06-25 20:26:53 +1000

    feat: user password encryption on presave

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -5,6 +5,7 @@
 
 const mongoose = require("mongoose");
 const { commentSchema } = require("./CommentSchema");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
 	username: {
@@ -18,11 +19,11 @@ const userSchema = mongoose.Schema({
 		unique: false
 	},
 	password: {
-		type: String,
-		required: true,
+		type: String, 
+		required: true, 
 		unique: false
 	}
-	// isAdmin // boolean for role authorization
+	// isAdmin // boolean for role authorization 
 	// comments: {
 	// 	// These are NOT the same comments as what the Blogs contain, they just reuse the comment schema
 	// 	types: [commentSchema],
@@ -36,23 +37,27 @@ userSchema.pre(
 	async function (next) { 
 		const user = this;
 		console.log("Pre-save hook running.");
-		
+
 		if (!user.isModified("password")){
-			return;
+			return next();
 		}
-		
-		console.log("Pre-save hook running and password is modified!");
+
+		console.log("Pre-save hook runnin and password is modified!");
 		// if we reach this line of code, the password is modified
-		// and thus is note encrypted!
+		// and thus is not encrypted!
 		// we must encrypt it!
 
-		// TODO: Encryption
-
+		console.log("Raw password is: " + this.password);
 		
+		const hash = await bcrypt.hash(this.password, 10);
+
+		console.log("Hashed and encrypted and salted password is: " + hash)
+
+		this.password = hash;
+
 		next();
 	}
 )
-
 
 
 
