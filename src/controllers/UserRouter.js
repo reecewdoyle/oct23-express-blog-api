376281a34af926d49ad338a14b57d34d6c485240
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-06-29 11:03:11 +1000

    fix: signing up automatically signs in

--- src/controllers/UserRouter.js
+++ src/controllers/UserRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { UserModel } = require("../models/UserModel"); // Need this imported somewhere in the server code to make the server connection use Users
+const { comparePasswords, createJwt } = require("../utils/authHelpers");
 const router = express.Router();
 
 router.get("/", async (request, response, next) => {
@@ -15,7 +16,7 @@ router.get("/", async (request, response, next) => {
 
 router.get("/findById/:id", async (request, response, next) => {
 
-	let result = await UserModel.findById(request.params.id).populate("author").exec();
+	let result = await UserModel.findById(request.params.id).exec();
 
 	response.json({
 		message:"User router operation",
@@ -25,7 +26,7 @@ router.get("/findById/:id", async (request, response, next) => {
 
 router.post("/findOneQuery", async (request, response, next) => {
 
-	let result = await UserModel.findOne(request.body).populate("author").exec();
+	let result = await UserModel.findOne(request.body).exec();
 
 	response.json({
 		message:"User router operation",
@@ -35,7 +36,7 @@ router.post("/findOneQuery", async (request, response, next) => {
 
 router.post("/findManyQuery", async (request, response, next) => {
 
-	let result = await UserModel.find(request.body).populate("author").exec();
+	let result = await UserModel.find(request.body).exec();
 
 	response.json({
 		message:"User router operation",
@@ -51,13 +52,16 @@ router.post("/", async (request, response, next) => {
 		return error
 	});
 
+	let jwt = createJwt(result._id);
+
 	if (result.errors) {
 		return next(result);
 	}
 
 	response.json({
 		message:"User router operation",
-		result: result
+		result: result,
+		jwt: jwt
 	});
 });
 
@@ -86,5 +90,42 @@ router.delete("/", async (request, response, next) => {
 		result: result
 	});
 });
+
+
+
+// Login route 
+router.post("/jwt", async (request, response, next) => {
+	let newJwt = "";
+
+	if (!request.body.password || !request.body.username){
+		return next(new Error("Missing login details in login request."));
+	}
+
+	// Find user by username in DB
+	let foundUser = await UserModel.findOne({username: request.body.username}).exec();
+
+	// Compare request.body.password to foundUser.password using the compare function 
+	let isPasswordCorrect = await comparePasswords(request.body.password, foundUser.password);
+
+
+	// Create a JWT based on foundUser._id 
+	if (isPasswordCorrect){
+
+		newJwt = createJwt(foundUser._id);
+
+		response.json({
+			jwt: newJwt
+		});
+	} else {
+		return next(new Error("Incorrect password."));
+	}
+
+	
+})
+
+// Validate JWT route 
+
+
+
 
 module.exports = router;
